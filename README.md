ef91c13f814922bc905dd4a0ecc16d27f60385f0
tungk1 <tobakonek1@gmail.com>
2023-02-07 19:33:51 +0000

    app init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Book Review Application
+
+This is a basic CRUD application, where you can register to add books you have read to the collection.
+Other users can also access the book collection and add a review to the book by registering.
+
+https://pure-refuge-19768.herokuapp.com/
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,52 @@
+class ReviewsController < ApplicationController
+  before_action :set_book
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  
+  def new
+    @review = Review.new
+  end
+  
+  def create
+    @review = Review.new(review_params)
+    @review.book_id = @book.id
+    @review.user_id = current_user.id
+    
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @review.update(review_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @review.destroy
+    redirect_to book_path(@book)
+  end
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+  
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
+  
+  def set_review
+    @review = Review.find(params[:id])
+  end
+  
+end
