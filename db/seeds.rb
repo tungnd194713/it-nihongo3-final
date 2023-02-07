15dabae2ee1a8686c0370562401b3df990c73fcf
tungk1 <tobakonek1@gmail.com>
2023-02-07 19:37:40 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = [  { name: 'Action' },  { name: 'Comedy' },  { name: 'Drama' },  { name: 'Thriller' },  { name: 'Horror' }, { name: 'Funny' }, { name: 'Asia' }, { name: 'Gay' }]
+
+Category.create(categories)
