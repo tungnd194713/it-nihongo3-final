19dddf506f7c14be4c607fd606728b3f7c8bc5b4
tungnd194713 <tung-nguyen@dimage.co.jp>
2023-02-08 16:02:17 +0700

    add docker

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews
-  has_attached_file :book_img, style: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :book_img, style: { book_index: "250x350>", book_show: "325x475>" }, default_url: "https://t4.ftcdn.net/jpg/00/89/55/15/360_F_89551596_LdHAZRwz3i4EM4J0NHNHy2hEUYDfXc0j.jpg"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
