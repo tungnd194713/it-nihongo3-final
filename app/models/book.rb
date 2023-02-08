62745bf92ab7f9fedc4845fc9181ee24b61a57cb
tungnd194713 <tung-nguyen@dimage.co.jp>
2023-02-08 16:12:36 +0700

    add realtime update file

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews
-  has_attached_file :book_img, style: { book_index: "250x350>", book_show: "325x475>" }, default_url: "https://t4.ftcdn.net/jpg/00/89/55/15/360_F_89551596_LdHAZRwz3i4EM4J0NHNHy2hEUYDfXc0j.jpg"
+  has_attached_file :book_img, style: { book_index: "250x350>", book_show: "325x475>" }, default_url: "https://d2j6dbq0eux0bg.cloudfront.net/images/24595415/1297476103.jpg"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
 end
